708eaf51ac1ca59e98c8bc3e6a326eef59ce3550
Croppedeyebrow <142734435+croppedeyebrow@users.noreply.github.com>
2026-05-09 11:04:13 +0900

    c_practcie

---
+++ C_practice/c_chartest2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+
+    char x = 'A';
+    char y = '1';
+
+    int z =1;
+
+    printf("x입력값 : %c, y입력값 : %c, z입력값 : %d\n", x, y, z);
+    //x,y는 문자(c)를 출력, z는 정수(int)를 출력
+
+    return 0;
+
+}
